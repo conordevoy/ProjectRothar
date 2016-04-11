5aa6d19ddcc51c395e44efc01610e2c0da903ebd
Conor Devoy <conor.devoy@ucdconnect.ie>
2016-04-11 14:29:24 +0100

    Station markers on map

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	// Create map
 	var mapDiv = document.getElementById("map_canvas");
-	// Map style array
 	var mapOptions = {
 		center: new google.maps.LatLng(53.347158, -6.262724), // Center map over Dublin
 		zoom: 14,
@@ -13,4 +12,32 @@ $(document).ready(function() {
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(mapDiv, mapOptions);
+
+	// Array: markers of stations with bikes available currently.
+	var markersAvalBikes = [];
+	// Array: markers of stations with spaces available currently.
+	var markersAvalSpaces = [];
+
+	// Request most recent update from API. 
+	$.getJSON("https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=a85c8f0702b254af2e0bddaf3a12603cf19579e4", function(json) {
+	/* Loop through JSON object: For each station: 
+	* place markers, 
+	* create and style infowindow, 
+	* create circles (size and colour indicative of the number of bikes available at station) 
+	*/
+		$.each(json, function(key, data) {
+			// Station lat and lng
+			var latLng = new google.maps.LatLng(data.position.lat, data.position.lng);  
+			// Create and set marker position 
+			var marker = new google.maps.Marker({
+				position: latLng,
+				icon: "/static/images/bike.png",
+				title: data.address,
+				draggable: false,
+			});
+			// Pass marker to map
+			marker.setMap(map);
+			// End of loop
+		});
+	});
 });
