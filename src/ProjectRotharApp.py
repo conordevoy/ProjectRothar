d39945a554e1429c9b9b2f8ac3daa10092aa9531
8ballbb <andrewpoolejames@gmail.com>
2016-04-14 13:36:38 +0100

    imports

--- src/ProjectRotharApp.py
+++ src/ProjectRotharApp.py
@@ -1,22 +1,20 @@
 from flask import Flask, render_template, request, g
+from flask.json import jsonify
+import json
 import sqlite3
 
 # Application object (set different path for static files on the web)
 projectRotharApp = Flask(__name__, static_url_path="/static")
-# Enable debugging of app
-projectRotharApp.debug = True
+# Enable debugging of app 
+projectRotharApp.debug = True 
 
 projectRotharApp.db = "projectRothar_DB.db"
 
-# Decorator used to register a view function for the url rule parameter
-# (Listens for only GET by default)
-
-
+# Decorator used to register a view function for the url rule parameter (Listens for only GET by default)
 @projectRotharApp.route("/")
 def ProjectRotharHTML():
     '''Render html template'''
     return render_template("ProjectRothar.html")
-
 
 @projectRotharApp.route("/historical_plot/<int:station_number>/<plot_day>")
 def historical_plot(station_number, plot_day):
@@ -26,14 +24,12 @@ def historical_plot(station_number, plot_day):
     '''
     # Connect to db and execute query
     g.db = connectDB()
-    cur = g.db.execute("select last_update, available_bikes from projectRothar_DB WHERE status = \"OPEN\" AND number = %d AND Scrape_Day = \"%s\"" % (
-        station_number, plot_day))
+    cur = g.db.execute("select last_update, available_bikes from projectRothar_DB WHERE status = \"OPEN\" AND number = %d AND Scrape_Day = \"%s\"" %(station_number, plot_day)) 
     # Create JSON object from query fetched
     data = json.dumps(cur.fetchall())
     # Close connection to db
-    g.db.close()
+    g.db.close() 
     return data
-
 
 def connectDB():
     '''Create and return a db object to interact with'''
