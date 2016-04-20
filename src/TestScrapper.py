6cbbe3ea8b285bb6c4c58b07f7194faf8a3316f0
Conor Devoy <conor.devoy@ucdconnect.ie>
2016-04-20 20:49:12 +0100

    remove test scrapper, not needed

--- src/TestScrapper.py
+++
@@ -1,8 +0,0 @@
-from Scrapper import *
-import json
-
-def test_API_connection():
-	API_return = DBStationInfo('https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=a85c8f0702b254af2e0bddaf3a12603cf19579e4')
-	print(API_return)
-
-test_API_connection()
